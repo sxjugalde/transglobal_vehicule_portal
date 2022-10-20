1c29aceba530db343f4efc09e9ea32360bc7b9c9
sxjugalde <joseph.ugalde@systems-x.com>
2022-10-20 11:42:45 -0600

    remove docker files and point db to local host

--- docker/run_celery.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#wait
-
-cd /app
-
-celery -A vehicle_service_portal worker -l INFO
--- docker/run_web.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#wait
-
-cd /app/
-
-#migrate
-python manage.py migrate --noinput
-python manage.py runserver 0:8000
